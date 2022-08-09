44d424f5f4e009df9b6f7755e2c63d134737eafe
Pete Fowler <fowler.pete@gmail.com>
2022-08-09 09:13:09 -0600

    commit tree e23e086aa841e85b9deece217a6ab8ff337ac205

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = (nm) => {
+  cats.push(nm);
+}
+
+const destructivelyPrependCat = (nm) => {
+  cats.unshift(nm);
+}
+
+const destructivelyRemoveLastCat = () => {
+  cats.pop();
+}
+
+const destructivelyRemoveFirstCat = () => {
+  cats.shift();
+}
+
+const appendCat = (nm) => {
+  const arr = [...cats];
+  arr.push(nm);
+  return arr;
+}
+
+const prependCat = (nm) => {
+  const arr = [...cats];
+  arr.unshift(nm);
+  return arr;
+}
+
+const removeLastCat = () => {
+  const arr = [...cats];
+  arr.pop();
+  return arr;
+}
+
+const removeFirstCat = () => {
+  const arr = [...cats];
+  arr.shift();
+  return arr;
+}
